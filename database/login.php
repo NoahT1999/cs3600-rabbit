f440b331f2d5cdd30feb299056e279c26832bb75
Josh Gillum <gill2092@vandals.uidaho.edu>
2025-10-27 02:03:34 -0700

    Added send_back_to query which will send the user back to that url

--- database/login.php
+++ database/login.php
@@ -25,7 +25,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             session_start();
             $_SESSION['user'] = $id;
             $_SESSION['username'] = $username;
-            header("Location: ../dashboard.php");
+            if(isset($_GET['send_back_to'])){
+              header("Location: ".$_GET['send_back_to']);
+            } else {
+              header("Location: ../dashboard.php");
+            }
             exit();
         } else {
             $message = "Incorrect password";
